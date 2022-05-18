b1fdf8a2784614d8ed358c37afd45d2286825943
vitorbcc2021 <102258365+vitorbcc2021@users.noreply.github.com>
2022-05-17 21:42:23 -0300

    Create Controlador.java

---
+++ Java/Encapsulamento/Controlador.java
@@ -0,0 +1,16 @@
+package encapsulamento;
+public interface Controlador {
+    
+    public abstract void ligar();
+    public abstract void desligar();
+    public abstract void abrirMenu();
+    public abstract void fecharMenu();
+    public abstract void maisVolume();
+    public abstract void menosVolume();
+    public abstract void mutar();
+    public abstract void desmutar();
+    public abstract void play();
+    public abstract void pause();
+    
+    
+}
