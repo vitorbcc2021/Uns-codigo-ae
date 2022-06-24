b5daa3726823d9e28ceef92b931bcae3b0f9f536
vitorbcc2021 <102258365+vitorbcc2021@users.noreply.github.com>
2022-06-24 13:52:36 -0300

    Update MACRO.asm

--- Assembly/MACRO.asm
+++ Assembly/MACRO.asm
@@ -7,114 +7,85 @@
 		la $a0, msg
 		syscall
 .end_macro
-
-
 ###########################################
-.macro printInteger()
+.macro printResultado()
 	.text
+		print("\nResultado: ")
+		
 		li $v0, 1
 		la $a0, ($t0)
 		syscall
 .end_macro
-
-
 ###########################################
-.macro scanInteger1()
-	.data
-		msg: .asciiz "Digite um valor inteiro: "
+.macro scanOpcao()
 	.text
-		li $v0, 4
-		la $a0, msg
-		syscall
-		
 		li $v0, 5
 		syscall
 		move $t1, $v0
 .end_macro
-
-
 ###########################################
-.macro scanInteger2()
-	.data
-		msg: .asciiz "Digite um valor inteiro: "
+.macro scanInteger()
 	.text
-		li $v0, 4
-		la $a0, msg
+		print("Digite o primeiro numero: ")
+		
+		li $v0, 5
 		syscall
+		move $t1, $v0
+		
+		print("Digite o segundo numero: ")
 		
 		li $v0, 5
 		syscall
 		move $t2, $v0
 .end_macro
-
-
 ###########################################
 .macro exit()
 	li $v0, 10
 	syscall
 .end_macro
-
-
 ################ MAIN #####################
 .text
 	.globl operacoes
 operacoes:
-	print("\n##OPERARCOES##\nDigite 0 para sair;\nDigite 1 para somar;\nDigite 2 para subtrair;\nDigite 3 para multiplicar;\nDigite 4 para dividir.\n")
-	scanInteger1()
+	print("\n\n##OPERARCOES##\nDigite 0 para sair;\nDigite 1 para somar;\nDigite 2 para subtrair;\nDigite 3 para multiplicar;\nDigite 4 para dividir.\n")
+	scanOpcao()
 
-	beq $t1, 0, sair
+	beq $v0, 0, sair
 	jal opcoes
 	j operacoes
 
 opcoes:
-	beq $t1, 1, adicao
-	beq $t1, 2, subtracao
-	beq $t1, 3, multiplicacao
-	beq $t1, 4, divisao
+	beq $v0, 1, adicao
+	beq $v0, 2, subtracao
+	beq $v0, 3, multiplicacao
+	beq $v0, 4, divisao
 
 adicao:
-	scanInteger1()
-	scanInteger2()
-
+	scanInteger()
 	add $t0, $t1, $t2
 
-	print("Resultado: ")
-	printInteger()
-
+	printResultado()
 	jr $ra
 
 subtracao:
-	scanInteger1()
-	scanInteger2()
-
+	scanInteger()
 	sub $t0, $t1, $t2
 
-	print("Resultado: ")
-	printInteger()	
-
+	printResultado()
 	jr $ra
 
 multiplicacao:
-	scanInteger1()
-	scanInteger2()
-
+	scanInteger()
 	mul $t0, $t1, $t2
 
-	print("Resultado: ")
-	printInteger()
-
+	printResultado()
 	jr $ra
 
 divisao:
-	scanInteger1()
-	scanInteger2()
-
+	scanInteger()
 	div $t0, $t1, $t2
-
-	print("Resultado: ")
-	printInteger()
-
+	
+	printResultado()
 	jr $ra
 
-sair:
-	exit()
+sair: exit()
